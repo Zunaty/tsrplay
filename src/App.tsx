b651be87219fecc8c6a6710702acc702751c102f
Victor Perez <zunaty@gmail.com>
2022-03-19 23:39:32 -0600

    fixed withdraw to not go under 0, added input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators, State } from './utils/globalState';
@@ -10,14 +9,37 @@ function App() {
   const dispatch = useDispatch();
 
   const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(actionCreators, dispatch);
-  const amount = useSelector((state: State) => state.bank)
+  const amount = useSelector((state: State) => state.bank);
+  const [moneyAmount, setMoneyAmount] = useState("");
+
+  // type PracObjectType = {
+  //   ldjflk: string;
+  //   fadf: number;
+  // }
+
+  const withdrawAmount = (leavingAmount: number) => {
+    if (amount === 0 || amount - leavingAmount < 0) {
+      return 0;
+    } else {
+      return leavingAmount;
+    }
+  };
+
+  const stringToNum = (moneyInput: string) => {
+    let x = parseInt(moneyInput)
+    return x;
+  };
 
 
   return (
     <div className="App">
       <h1>Current Balance: {amount}</h1>
-      <button onClick={() => depositMoney(100)}>Deposit</button>
-      <button onClick={() => withdrawMoney(100)}>Withdraw</button>
+      <input
+        value={moneyAmount}
+        onChange={(evt) => setMoneyAmount(evt.target.value)}
+      />
+      <button onClick={() => depositMoney(stringToNum(moneyAmount))}>Deposit</button>
+      <button onClick={() => withdrawMoney(withdrawAmount(stringToNum(moneyAmount)))}>Withdraw</button>
       <button onClick={() => bankrupt()}>Bankrupt</button>
     </div>
   );
